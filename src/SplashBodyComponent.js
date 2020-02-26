81f1579071ee4d0ae8281226beffcf4abac4705c
Neil <neilkelkar1@gmail.com>
2020-02-26 14:36:39 -0500

    Completed general splash layout 'look'

--- src/SplashBodyComponent.js
+++ src/SplashBodyComponent.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import './SplashBodyComponent.css';
 
 export default function SplashBodyComponent() {
     return (
+        <div class='bodySection'>
+        <img src={require('./smiling.jpg')} alt='SmilingStudent' id='mainImage3'/>
+        <img src={require('./laptop.jpg')} alt='Looking at Laptop' id='mainImage2'/>
         <img src={require('./students.jpg')} alt='Group of Students' id='mainImage'/>
+        
+        <div class = 'welcomeTextBlock'>
+            <span class = 'welcomeTextHeading'>
+                Find the right university for you.
+            </span>
+            <span class = 'welcomeText'>
+                Using your grades along with your location and program preferences,
+                we search through universities and colleges that match your needs.
+            </span>
+        </div>
+        <div class = 'welcomeTextBlock2'>
+            <span class = 'welcomeTextHeading'>
+                Find the right university for you.
+            </span>
+            <span class = 'welcomeText'>
+                Using your grades along with your location and program preferences,
+                we search through universities and colleges that match your needs.
+            </span>
+        </div>
+        <div class = 'welcomeTextBlock'>
+            <span class = 'welcomeTextHeading'>
+                Find the right university for you.
+            </span>
+            <span class = 'welcomeText'>
+                Using your grades along with your location and program preferences,
+                we search through universities and colleges that match your needs.
+            </span>
+        </div>
+        </div>
     )
 }
