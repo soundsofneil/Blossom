cae308441e9a0689508535214043d78b8f6ab015
Jacob Erickson <jarus.erickson@gmail.com>
2020-03-23 18:34:59 -0400

    update models

--- models/program-requirements.js
+++ models/program-requirements.js
@@ -4,5 +4,11 @@ const GradeSchema = require('grade.js')
 
 module.exports = new mongoose.Schema({
     program: ProgramSchema,
-    gradeRequirement: GradeSchema
+    gradeRequirement: GradeSchema,
+    website: {
+        type: String,
+		required: true,
+        minlength: 1,
+        trim: true
+    }
 })
